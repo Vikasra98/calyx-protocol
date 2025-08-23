22f4365e13d36b59ab3e8916514c7294ac0ce19c
kdsvikas <kds.vikas@gmail.com>
2025-08-23 21:06:39 +0530

    fixed error

--- app/components/ContactMap.tsx
+++ app/components/ContactMap.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+"use client";
+
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
 
 const ContactMap = () => {
+  const center: LatLngExpression = [40.7128, -74.006]; // New York
+
   return (
-    <>
-      <div>hh</div>
-    </>
+    <div className="w-full h-[500px]">
+      <MapContainer
+        center={center}
+        zoom={10}
+        className="w-full h-full rounded-lg shadow-md"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+        />
+      </MapContainer>
+    </div>
   );
 };
 
